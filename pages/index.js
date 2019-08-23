36f86d139770d95c7568b9d8b6cfa3757b3f3dd4
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-23 09:52:02 +0100

    Initial Changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
 
 const Index = () => (
   <div>
-    <h1>Welcome!!</h1>
+    <h1>Welcome Sakthi!!</h1>
+    <Link href="/about">
+      <a title="about me">About page</a>
+    </Link>
   </div>
 );
 
